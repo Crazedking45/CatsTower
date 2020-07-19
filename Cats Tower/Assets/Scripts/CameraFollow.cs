3dbe7ad36c51161852273062ba9bb7c0126030ff
Jonpaul Gee <jonpaulgee@yahoo.com>
2020-07-19 08:43:47 -0700

    Camera Fix and Update

--- Cats Tower/Assets/Scripts/CameraFollow.cs
+++ Cats Tower/Assets/Scripts/CameraFollow.cs
@@ -4,16 +4,15 @@ public class CameraFollow : MonoBehaviour
 {
     public Transform dogTarget;
 
-    public float smoothTransition = 0.125f;
+    public float smoothSpeed = 0.125f;
     public Vector3 offset;
 
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        Vector3 desiredPosition = dogTarget.position + offset;
-        Vector3 smoothPosition = Vector3.Lerp(transform.position, desiredPosition, smoothTransition);
-        transform.position = smoothPosition;
 
-        transform.LookAt(dogTarget);
+    void LateUpdate()
+    {
+     
+        transform.position = dogTarget.position + offset;
+
+       
     }
 }
