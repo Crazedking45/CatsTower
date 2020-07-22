2267c990667536da3fbd33cca87f8d826b42a6c7
Samy Du <33183498+MKULTRAA@users.noreply.github.com>
2020-07-21 21:44:04 -0700

    AddedCoins

---
+++ Cats Tower/Assets/Biscuit Collectable/Coins/Script/CoinScoringSystem.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinScoringSystem : MonoBehaviour
+{
+    public GameObject scoringSytem;
+    public static int theScore;
+
+
+    void Update()
+    {
+        scoringSytem.GetComponent<Text>().text = "Coins: " + theScore;  //updating the score
+    }
+}
---
+++ Cats Tower/Assets/Biscuit Collectable/Coins/Script/CollectCoins.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectCoins : MonoBehaviour
+{
+    public AudioSource collectSound;
+    void OnTriggerEnter(Collider other)
+    {
+        //collectSound.Play();
+        CoinScoringSystem.theScore += 1;
+        GameObject.Destroy(gameObject);
+    }
+
+
+}
+
+  
+
