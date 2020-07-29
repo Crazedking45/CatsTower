ff6fa4aac8e2a385cd640a9557e17c84c372624e
Jonpaul Gee <jonpaulgee@yahoo.com>
2020-07-29 02:24:35 -0700

    Enemy AI
    Movement towards player isnt working for some reason it should

---
+++ Cats Tower/Assets/Scripts/CharacterStats.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class CharacterStats : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth { get; private set; }
+    public Stat damage;
+
+    void Awake()
+    {
+        currentHealth = maxHealth;
+    }
+   
+    void Update()
+    {
+        
+    }
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        if(currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    public virtual void Die()
+    {
+
+    }
+}
