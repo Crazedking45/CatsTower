a99547f6330ebc857135313a0a05d6b26e9dc24b
Jonpaul Gee <jonpaulgee@yahoo.com>
2020-08-10 04:18:28 -0700

    UI

--- Cats Tower/Assets/Scripts/CharacterStats.cs
+++ Cats Tower/Assets/Scripts/CharacterStats.cs
@@ -5,19 +5,25 @@ public class CharacterStats : MonoBehaviour
     public int maxHealth = 100;
     public int currentHealth { get; private set; }
     public Stat damage;
+    public HealthBar healthBar;
 
-    void Awake()
+    void Start()
     {
         currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
     }
    
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(20);
+        }
     }
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
 
         if(currentHealth <= 0)
         {
