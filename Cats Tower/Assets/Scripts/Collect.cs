9ece51064df62daec94afcd61d4f0c9a3a7586d3
Samy Du <33183498+MKULTRAA@users.noreply.github.com>
2020-08-11 23:32:11 -0700

    WIP Coin Collecting for Chest Room

---
+++ Cats Tower/Assets/Scripts/Collect.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Collect : MonoBehaviour
+{
+    private int count;
+    public GameObject TriggerObj;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        count = 0;
+        TriggerObj.gameObject.SetActive(true);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        if(count == 20)
+        {
+            TriggerObj.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Cats Tower/Assets/Scripts/DoorOpen.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpen : MonoBehaviour
+{
+    public Animation DoorAni;
+
+
+    public void OnTriggerEnter(Collider other)
+    {
+        DoorAni.Play();
+    }
+}
