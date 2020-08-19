81bd9190d7e4fe5a0e54b4ebe9979e94cedcaa50
CQoder29 <41121928+CQoder29@users.noreply.github.com>
2020-08-19 01:46:49 -0700

    um dumb
    no you

--- Cats Tower/Assets/Scripts/Collect.cs
+++ Cats Tower/Assets/Scripts/Collect.cs
@@ -5,13 +5,17 @@ using System.Collections;
 public class Collect : MonoBehaviour
 {
     public Text countText;
+    public Text keyCountText;
     public Text DoorOpenText;
     private int count;
+    private int keyCount;
     public Text collectItem;
     public GameObject TriggerObj;
     public GameObject uiObject;
     public GameObject TextTrigger;
     public GameObject DoorUnlockedText;
+
+    public GameObject DoorVanish;
 
     public float timer = 3;
 
@@ -19,8 +23,10 @@ public class Collect : MonoBehaviour
     void Start()
     {
         count = 0;
+        keyCount = 0;
         DoorOpenText.text = "";
         SetCountText();
+        SetKeyCountText();
         collectItem.gameObject.SetActive(true);
         TriggerObj.gameObject.SetActive(true);
         uiObject.SetActive(false);
@@ -34,21 +40,38 @@ public class Collect : MonoBehaviour
             count = count + 1;
             SetCountText();
         }
+
+        if (other.gameObject.CompareTag("key"))
+        {
+            other.gameObject.SetActive(false);
+            keyCount = keyCount + 1;
+            SetKeyCountText();
+        }
     }
 
     void SetCountText()
     {
-        countText.text = "Coint: " + countText.ToString();
-        if(count == 20)
+        countText.text = "Coin: " + countText.ToString();
+
+        if (count == 20)
         {
             DoorOpenText.text = "Door Unlocked!";
             collectItem.gameObject.SetActive(false);
             TriggerObj.gameObject.SetActive(false);
             count = count - 20;
-            countText.text = "Coint: " + countText.ToString();
+            countText.text = "Coin: " + countText.ToString();
             TextTrigger.gameObject.SetActive(false);
 
             Destroy(DoorUnlockedText, timer);
+        }
+    }
+
+    void SetKeyCountText()
+    {
+        if (keyCount >= 1)
+        {
+            DoorVanish.SetActive(false);
+
         }
     }
 }
