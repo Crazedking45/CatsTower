c78948fbdb3fbeb964565d0762019c4bbbbfc14c
CQoder29 <41121928+CQoder29@users.noreply.github.com>
2020-08-19 01:39:03 -0700

    um thing
    insert more things
    
    door just graduated
    no longer dummythicc
    
    sam smells

---
+++ Cats Tower/Assets/Scripts/HiddenBlockSpawn.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HiddenBlockSpawn : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public GameObject key;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("player"))
+        {
+            audioSource.Play();
+        }
+
+        else
+        {
+            this.gameObject.GetComponent<Renderer>().enabled = false;
+            key.SetActive(false);
+        }
+
+        if (other.gameObject.CompareTag("Weapon"))
+        {
+            this.gameObject.GetComponent<Renderer>().enabled = true;
+            key.SetActive(true);
+        }
+
+        else
+        {
+            this.gameObject.GetComponent<Renderer>().enabled = false;
+            key.SetActive(false);
+        }
+    }
+}
