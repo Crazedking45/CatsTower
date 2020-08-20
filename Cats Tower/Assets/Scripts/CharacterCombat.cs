51185cb8106c6edc5d77b963c62201c896f783cc
Jonpaul <jonpaulgee@yahoo.com>
2020-08-20 09:44:49 -0700

    Pass

--- Cats Tower/Assets/Scripts/CharacterCombat.cs
+++ Cats Tower/Assets/Scripts/CharacterCombat.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+//using System.Diagnostics;
 using UnityEngine;
 
 [RequireComponent(typeof(CharacterStats))]
@@ -7,13 +8,41 @@ using UnityEngine;
 public class CharacterCombat : MonoBehaviour
 {
     CharacterStats myStats;
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayers;
+    
     void Start()
     {
         myStats = GetComponent<CharacterStats>();
 
     }
+    
    public void Attack (CharacterStats targetStats)
     {
         targetStats.TakeDamage(myStats.damage.GetValue());
+
+        Collider[] hitEnemies = Physics.OverlapSphere(attackPoint.position, attackRange,enemyLayers);
+
+        foreach(Collider enemy in hitEnemies)
+        {
+            Debug.Log("We hit " + enemy.name);
+        }
+        
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(0))
+        {
+            Attack(myStats);
+        }
+    }
+    void onDrawGizmosSelected()
+    {
+        if(attackPoint == null)
+        {
+            return;
+        }
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
     }
 }
